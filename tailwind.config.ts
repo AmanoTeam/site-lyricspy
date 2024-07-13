8de1418445e132623d989a3adb30387dae277972
Alisson Lauffer <alissonvitortc@gmail.com>
2024-07-12 23:42:19 -0300

    use weight 700 font and refactor ts a bit, using consts where possible

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
